91285e845aa510a9888ddcd28e0c0e5e6f83876c
ken <block24block@gmail.com>
2018-12-19 00:12:36 +0800

    init

---
+++ main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"path/filepath"
+)
+
+type config struct {
+	Interval int             `json:"interval"`
+	JSON     json.RawMessage `json:"json"`
+}
+
+func main() {
+	for _, cfg := range configs() {
+		// go func(js []byte) {
+		// jsonReader := bytes.NewReader(json)
+		// fetch(jsonReader)
+		jsonReader := bytes.NewReader(cfg.JSON)
+		fetch(jsonReader)
+		// }(js)
+	}
+	// <-make(chan bool)
+}
+
+func configs() []config {
+	var configArray []config
+
+	files, err := filepath.Glob("configs/*")
+	if err != nil {
+		panic(err)
+	}
+
+	for _, file := range files {
+		js, err := ioutil.ReadFile(file)
+		if err != nil {
+			panic(err)
+		}
+
+		cfg := config{}
+		if err = json.Unmarshal(js, &cfg); err != nil {
+			panic(err)
+		}
+		configArray = append(configArray, cfg)
+	}
+
+	return configArray
+}
+
+func fetch(json io.Reader) {
+	req, _ := http.NewRequest("GET", "http://192.168.0.107:9200/_search", json)
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	fmt.Println("response Status:", resp.Status)
+	fmt.Println("response Headers:", resp.Header)
+	body, _ := ioutil.ReadAll(resp.Body)
+	fmt.Println("response Body:", string(body))
+
+}
