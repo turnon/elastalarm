57350f7f8229cf90f01924d0980893d889591d49
ken <block24block@gmail.com>
2019-01-08 15:36:54 +0800

    handle multi levels filter in aggs

---
+++ response/aggs.go
@@ -0,0 +1,119 @@
+package response
+
+import (
+	"encoding/json"
+	"strconv"
+)
+
+type singleAggName struct {
+	prefix, name string
+}
+
+type singleAgg struct {
+	DocCountErrorUpperBound int             `json:"doc_count_error_upper_bound"`
+	SumOtherDocCount        int             `json:"sum_other_doc_count"`
+	RawBuckets              json.RawMessage `json:"buckets"`
+	_buckets                []*bucket
+}
+
+func (a *singleAgg) buckets() []*bucket {
+	if a._buckets == nil {
+		var (
+			bs         []*bucket
+			rawBuckets []json.RawMessage
+		)
+		json.Unmarshal(a.RawBuckets, &rawBuckets)
+		for _, rawBucket := range rawBuckets {
+			b := &bucket{}
+			b.unmarshal(rawBucket)
+			bs = append(bs, b)
+		}
+		a._buckets = bs
+	}
+
+	return a._buckets
+}
+
+const wrongKey = "wrong_key"
+
+type bucket struct {
+	key             string
+	docCount        int
+	nameRawAggPairs map[string](*json.RawMessage)
+	nameAggPairs    map[singleAggName](*singleAgg)
+}
+
+func (b *bucket) unmarshal(bytes []byte) {
+	var nameRawAggPairs map[string](*json.RawMessage)
+	json.Unmarshal(bytes, &nameRawAggPairs)
+
+	for key, rawMsg := range nameRawAggPairs {
+		if key == "key" {
+			b.key = string(*rawMsg)
+		} else if key == "doc_count" {
+			count, _ := strconv.Atoi(string(*rawMsg))
+			b.docCount = count
+		} else {
+			if b.nameRawAggPairs == nil {
+				b.nameRawAggPairs = make(map[string](*json.RawMessage))
+			}
+			b.nameRawAggPairs[key] = rawMsg
+		}
+	}
+}
+
+func (b *bucket) aggs() *map[singleAggName]*singleAgg {
+	if b.nameAggPairs == nil {
+		b.cacheAggs()
+	}
+	return &b.nameAggPairs
+}
+
+func (b *bucket) cacheAggs() {
+	aggs := make(map[singleAggName]*singleAgg)
+
+	for key, rawMsgAddr := range b.nameRawAggPairs {
+		var tryUnmarshal map[string]*json.RawMessage
+		json.Unmarshal(*rawMsgAddr, &tryUnmarshal)
+
+		if tryUnmarshal["doc_count"] == nil {
+			name := singleAggName{name: key}
+			agg := &singleAgg{}
+			json.Unmarshal(*rawMsgAddr, agg)
+			aggs[name] = agg
+		} else {
+			var filter bucket
+			filter.unmarshal(*rawMsgAddr)
+			for name, agg := range *filter.aggs() {
+				name.prefix = key
+				aggs[name] = agg
+			}
+		}
+	}
+
+	b.nameAggPairs = aggs
+}
+
+func (b *bucket) flatten(f func([]interface{}, int)) {
+	var keys []interface{}
+	b._flatten(keys, 0, f)
+}
+
+func (b *bucket) _flatten(keys []interface{}, count int, f func([]interface{}, int)) {
+	aggs := *b.aggs()
+	if len(aggs) == 0 {
+		f(keys, count)
+		return
+	}
+
+	for name, a := range aggs {
+		for _, b := range a.buckets() {
+			key := b.key
+			if name.prefix != "" {
+				key = name.prefix + "/" + key
+			}
+			moreKeys := append(keys, key)
+			b._flatten(moreKeys, b.docCount, f)
+		}
+	}
+}
--- response/response.go
+++ response/response.go
@@ -24,74 +24,6 @@ type hit struct {
 	Source json.RawMessage `json:"_source"`
 }
 
-type singleAgg struct {
-	DocCountErrorUpperBound int      `json:"doc_count_error_upper_bound"`
-	SumOtherDocCount        int      `json:"sum_other_doc_count"`
-	Buckets                 []bucket `json:"buckets"`
-}
-
-type bucket map[string](*json.RawMessage)
-
-func (b *bucket) key() string {
-	if rawMsgAddr := (*b)["key"]; rawMsgAddr != nil {
-		return string(*rawMsgAddr)
-	}
-	return ""
-}
-
-func (b *bucket) docCount() int {
-	if rawMsgAddr := (*b)["doc_count"]; rawMsgAddr != nil {
-		i, _ := strconv.Atoi(string(*rawMsgAddr))
-		return i
-	}
-	return 0
-}
-
-func (b *bucket) aggs() *map[string]*singleAgg {
-	aggs := make(map[string]*singleAgg)
-
-	for key, rawMsgAddr := range *b {
-		if key == "key" || key == "doc_count" {
-			continue
-		}
-
-		var tryUnmarshal map[string]*json.RawMessage
-		json.Unmarshal(*rawMsgAddr, &tryUnmarshal)
-
-		if tryUnmarshal["doc_count"] == nil {
-			agg := &singleAgg{}
-			json.Unmarshal(*rawMsgAddr, agg)
-			aggs[key] = agg
-		} else {
-			var subB bucket
-			json.Unmarshal(*rawMsgAddr, &subB)
-			return subB.aggs()
-		}
-	}
-
-	return &aggs
-}
-
-func (b *bucket) flatten(f func([]interface{}, int)) {
-	var keys []interface{}
-	b._flatten(keys, 0, f)
-}
-
-func (b *bucket) _flatten(keys []interface{}, count int, f func([]interface{}, int)) {
-	aggs := *b.aggs()
-	if len(aggs) == 0 {
-		f(keys, count)
-		return
-	}
-
-	for _, a := range aggs {
-		for _, b := range a.Buckets {
-			moreKeys := append(keys, b.key())
-			b._flatten(moreKeys, b.docCount(), f)
-		}
-	}
-}
-
 func (resp *Response) Unmarshal(js []byte) {
 	if err := json.Unmarshal(js, resp); err != nil {
 		panic(err)
@@ -111,7 +43,7 @@ func (resp *Response) FlattenAggs() string {
 		b  bucket
 		sb strings.Builder
 	)
-	json.Unmarshal(resp.Aggregations, &b)
+	b.unmarshal(resp.Aggregations)
 
 	b.flatten(func(arr []interface{}, count int) {
 		sb.WriteString(fmt.Sprint(arr))
